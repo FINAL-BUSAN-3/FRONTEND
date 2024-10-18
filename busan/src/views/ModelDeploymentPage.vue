8b83a8ac29ef8793c7cf1725e006eb2021b4dbdf
Basil-Pesto-Pizza <gjhsh9077@naver.com>
2024-10-18 17:18:27 +0900

    <ModelDeploymentPage.vue 제작1> 선택하는 것만 구현 - 예시 데이터는 각 component에 담음
    <Components 파일 네이밍 다시함>
    - 이름 짓는 방법 Views랑 통일

--- busan/src/views/ModelDeploymentPage.vue
+++ busan/src/views/ModelDeploymentPage.vue
@@ -1,34 +1,108 @@
 <template>
   <div class="model-deployment">
-    <h2>모델 배포</h2>
+    <div v-if="!uploadSuccess">
+      <h2>모델 배포</h2>
 
-    <!-- 공정 선택 -->
-    <div class="process-section">
-      <label for="process-select">공정</label>
-      <select id="process-select" v-model="selectedProcess">
-        <option value="" disabled>공정을 선택해 주세요</option>
-        <option v-for="process in processes" :key="process" :value="process">{{ process }}</option>
-      </select>
+      <div class="selection-container">
+        <!-- 공정 선택 -->
+        <div class="process-section">
+          <label for="process-select">공정</label>
+          <select id="process-select" v-model="selectedProcess">
+            <option value="" disabled>공정을 선택해 주세요</option>
+            <option v-for="process in processes" :key="process" :value="process">{{ process }}</option>
+          </select>
+        </div>
+
+        <!-- 모델 선택 -->
+        <div class="model-section">
+          <label for="model-select">모델</label>
+          <select id="model-select" v-model="selectedModel">
+            <option value="" disabled>모델을 선택해 주세요</option>
+            <option v-for="model in models" :key="model" :value="model">{{ model }}</option>
+          </select>
+        </div>
+      </div>
+
+      <!-- 파일 업로드 기능 -->
+      <div class="file-upload">
+        <label for="file">모델 파일 업로드:</label>
+        <input type="file" id="file" @change="handleFileUpload" ref="fileInput" />
+        <button v-if="file" @click="clearFile" class="clear-button">X</button>
+      </div>
+
+      <!-- 업로드 버튼 -->
+      <ModelInsertUpload @click="uploadModel" />
     </div>
 
-    <!-- 모델 선택 -->
-    <div class="model-section">
-      <label for="model-select">모델</label>
-      <select id="model-select" v-model="selectedModel" >
-        <option value="" disabled>모델을 선택해 주세요</option>
-        <option v-for="model in models" :key="model" :value="model">{{ model }}</option>
-      </select>
-    </div>
+    <div v-else>
+      <h2>모델 비교</h2>
+      <div class="model-comparison">
+        <div class="model-detail left">
+          <div class="model-info">
+            <p>* 모델명: {{ modelDetails[0].name }}</p>
+            <p>* 모델 버전: {{ modelDetails[0].version }}</p>
+            <p>* 파일생성일자: {{ modelDetails[0].fileDate }}</p>
+            <p>* 파일크기: {{ modelDetails[0].fileSize }}</p>
+            <p>* 파일프레임워크: {{ modelDetails[0].framework }}</p>
+            <p>* 학습 정보: {{ modelDetails[0].trainingInfo }}</p>
+            <p>* 배포일자: {{ modelDetails[0].deploymentDate }}</p>
+          </div>
+          <div class="model-stats">
+            <table>
+              <thead>
+                <tr>
+                  <th>항목</th>
+                  <th>값</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Loss</td>
+                  <td>{{ modelDetails[0].avgLoss }}</td>
+                </tr>
+                <tr>
+                  <td>Accuracy</td>
+                  <td>{{ modelDetails[0].avgAccuracy }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
 
-    <!-- 파일 업로드 기능 -->
-    <div class="file-upload">
-      <label for="file">모델 파일 업로드:</label>
-      <input type="file" id="file" @change="handleFileUpload" ref="fileInput" />
-      <button v-if="file" @click="clearFile" class="clear-button">X</button>
+        <div class="model-detail right">
+          <div class="model-info">
+            <p>* 모델명: {{ modelDetails[1].name }}</p>
+            <p>* 모델 버전: {{ modelDetails[1].version }}</p>
+            <p>* 파일생성일자: {{ modelDetails[1].fileDate }}</p>
+            <p>* 파일크기: {{ modelDetails[1].fileSize }}</p>
+            <p>* 파일프레임워크: {{ modelDetails[1].framework }}</p>
+            <p>* 학습 정보: {{ modelDetails[1].trainingInfo }}</p>
+            <p>* 배포일자: {{ modelDetails[1].deploymentDate }}</p>
+          </div>
+          <div class="model-stats">
+            <table>
+              <thead>
+                <tr>
+                  <th>항목</th>
+                  <th>값</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Loss</td>
+                  <td>{{ modelDetails[1].avgLoss }}</td>
+                </tr>
+                <tr>
+                  <td>Accuracy</td>
+                  <td>{{ modelDetails[1].avgAccuracy }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <button @click="deployModel" class="deploy-button">배포</button>
     </div>
-
-    <!-- 업로드 버튼 -->
-    <ModelInsertUpload @click="uploadModel" />
   </div>
 </template>
 
@@ -36,6 +110,7 @@
 import ModelInsertUpload from '@/components/model_deployment/ModelInsert.vue';
 import ProcessSelectComponent from '@/components/model_deployment/ProcessSelect.vue';
 import ModelSelectComponent from '@/components/model_deployment/ModelSelect.vue';
+import ModelDetailData from '@/components/model_deployment/ModelDetail.vue';
 
 export default {
   name: 'ModelDeploymentPage',
@@ -44,7 +119,8 @@ export default {
   },
   data() {
     return {
-      selectedProcess: '',
+      uploadSuccess: false,
+      modelDetails: ModelDetailData.data().modelDetails,
       selectedModel: '',
       file: null,
       processes: ProcessSelectComponent.data().items,
@@ -58,11 +134,20 @@ export default {
   },
   methods: {
     uploadModel() {
-      if (!this.selectedModel && !this.file) {
-        alert('업로드할 모델 또는 파일을 선택해 주세요.');
+      if (!this.selectedModel) {
+        alert('업로드 실패: 모델을 선택해 주세요.');
         return;
       }
+      if (!this.file) {
+        alert('업로드 실패: 파일을 업로드해 주세요.');
+        return;
+      }
+      this.uploadSuccess = true;
       alert('업로드 성공!');
+    },
+    deployModel() {
+      alert('배포 성공!');
+      this.uploadSuccess = false;
     },
     clearFile() {
       this.file = null;
@@ -70,10 +155,10 @@ export default {
         this.$refs.fileInput.value = null;
       }
     },
-        handleFileUpload(event) {
+    handleFileUpload(event) {
       if (event.target.files.length > 0) {
         this.file = event.target.files[0];
-              }
+      }
     }
   },
 };
@@ -93,11 +178,33 @@ export default {
   justify-content: center;
 }
 
+.selection-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%; /* 선택창의 전체 너비 조정 */
+  gap: 20px;
+}
+
 .process-section,
 .model-section,
 .file-upload {
   margin-bottom: 20px;
-  width: 50%;
+  width: 100%; /* 파일 업로드와 동일한 너비 설정 */
+}
+
+.model-comparison {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.model-detail {
+  width: 48%;
+  background-color: #3a3f47;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 }
 
 label {
@@ -122,5 +229,16 @@ input[type="file"] {
   padding: 5px;
   cursor: pointer;
   margin-left: 10px;
+}
+
+.deploy-button {
+  margin-top: 20px;
+  background-color: #4caf50;
+  color: white;
+  padding: 10px 20px;
+  text-decoration: none;
+  border-radius: 5px;
+  text-align: center;
+  cursor: pointer;
 }
 </style>
