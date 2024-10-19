0577e1911161d37fffe2b872a7d2dc489ebd36a9
2948351 <shining@kia.com>
2024-10-19 17:47:54 +0900

    cicd

--- web_killer.sh
+++ web_killer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 'app'을 포함한 프로세스 ID를 가져옵니다.
-pids=$(ps -ef | grep vue | awk '{print $2}')
+pids=$(ps -ef | grep vue | grep -v grep | awk '{print $2}')
 
 # 프로세스 ID가 존재하는 경우에만 종료합니다.
 if [ -n "$pids" ]; then
