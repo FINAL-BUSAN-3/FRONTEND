7ec81e121d60de6ab58aca18b180df72e3516985
2948351 <shining@kia.com>
2024-10-19 17:19:35 +0900

    CI/CD

---
+++ vue_killer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 'app'을 포함한 프로세스 ID를 가져옵니다.
+pids=$(ps -ef | grep vue | awk '{print $2}')
+
+# 프로세스 ID가 존재하는 경우에만 종료합니다.
+if [ -n "$pids" ]; then
+    for pid in $pids; do
+        echo "Killing process $pid"
+        kill "$pid"
+    done
+else
+    echo "No processes found for 'app'."
+fi
