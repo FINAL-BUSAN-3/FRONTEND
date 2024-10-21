307fc25e3907bc80cff6404a2fb73b5aef133743
Basil-Pesto-Pizza <gjhsh9077@naver.com>
2024-10-21 17:26:26 +0900

    <파일 구조 변경> 1) components 라우팅 안 되는 문제 해결 2) 커밋 시 충돌 예방을 위해 url별 파일 구분 3) 희림 + 수현 합침 4) user-list에서 데이터 가져오는 거 성공
    ==> 이제 개별작업해도 충돌 안 생김

--- busan/vue.config.js
+++ busan/vue.config.js
@@ -1,17 +1,8 @@
-const path = require('path');
-
-module.exports = {
-  transpileDependencies: [
-    // 필요한 경우 여기에 transpile할 패키지 이름을 추가하세요
-  ],
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
   devServer: {
-    allowedHosts: "all"
-  },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'), // '@'를 'src' 디렉토리로 설정
-      },
-    },
-  },
-};
+        allowedHosts: "all"
+    }
+	}
+)
